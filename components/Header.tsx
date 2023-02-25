16f9b2348368b0d042f879fceff4c23c09a10e2c
Emmanuel Hernandez <e805177@gmail.com>
2023-02-24 21:14:11 -0600

    1.1.14 some changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -51,19 +51,22 @@ export const Header: FC = ({}) => {
                     <div className="hidden md:block">
                       <div className="ml-10 flex items-baseline space-x-4">
                         {navigation.map((item) => (
-                          <a
-                            key={item.name}
-                            href={item.href}
-                            className={classNames(
-                              item.current
-                                ? "bg-gray-900 text-white"
-                                : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                              "px-3 py-2 rounded-md text-sm font-medium"
-                            )}
-                            aria-current={item.current ? "page" : undefined}
-                          >
-                            {item.name}
-                          </a>
+                          <Link href={item.href}>
+                          <span
+                          key={item.name}
+                          
+                          className={classNames(
+                            item.current
+                              ? "bg-gray-900 text-white"
+                              : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                            "px-3 py-2 rounded-md text-sm font-medium"
+                          )}
+                          aria-current={item.current ? "page" : undefined}
+                        >
+                          {item.name}
+                        </span>
+                          </Link>
+  
                         ))}
                       </div>
                     </div>
