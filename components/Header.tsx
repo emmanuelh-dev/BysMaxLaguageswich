ee85b289d35881a84592b6e20de3b31503eed22e
Emmanuel Hernandez <e805177@gmail.com>
2023-02-24 18:35:35 -0600

    1.12.1 un poco de estilos

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const user = {
-  name: "Tom Cook",
+  name: "Calculadoras",
   email: "tom@example.com",
   imageUrl:
     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
@@ -43,11 +43,9 @@ export const Header: FC = ({}) => {
                 <div className="flex h-16 items-center justify-between">
                   <div className="flex items-center">
                     <div className="flex-shrink-0">
-                      <img
-                        className="h-8 w-8"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                        alt="Your Company"
-                      />
+                      <Link href="/">
+                        <span className="font-bold text-lg hover:text-blue-700">BysMax</span>
+                      </Link>
                     </div>
                     <div className="hidden md:block">
                       <div className="ml-10 flex items-baseline space-x-4">
@@ -156,20 +154,10 @@ export const Header: FC = ({}) => {
                   ))}
                 </div>
                 <div className="border-t border-gray-700 pt-4 pb-3">
-                  <div className="flex items-center px-5">
-                    <div className="flex-shrink-0">
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src={user.imageUrl}
-                        alt=""
-                      />
-                    </div>
+                  <div className="flex items-center px-2">
                     <div className="ml-3">
                       <div className="text-base font-medium leading-none text-white">
                         {user.name}
-                      </div>
-                      <div className="text-sm font-medium leading-none text-gray-400">
-                        {user.email}
                       </div>
                     </div>
                     <button
