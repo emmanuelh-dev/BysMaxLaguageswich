62d10d19b59b4a2006d1855400e08fb76be124ce
Emmanuel Hernandez <e805177@gmail.com>
2023-02-26 00:04:21 -0600

    1.4.6 anatalytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
+import { Analytics } from '@vercel/analytics/react';
 
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 // import nextI18NextConfig from '../next-i18next.config.js'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
+  <>
   <Component {...pageProps} />
+  <Analytics />
+  </>
 )
 
 // https://github.com/i18next/next-i18next#unserializable-configs
