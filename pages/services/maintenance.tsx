f946ed29e89c6e2d963f4997876283f5215707b3
emmanuelh-dev <86961961+emmanuelh-dev@users.noreply.github.com>
2023-03-06 23:57:34 -0600

    1.2.28 maintenance

--- pages/services/maintenance.tsx
+++ pages/services/maintenance.tsx
@@ -16,7 +16,7 @@ const services = (
 ) => {
   const { t } = useTranslation(["homepage"]);
   const router = useRouter();
-  const pageTitle = `Mantenimiento en ${router.query.city || 'Todo Nuevo Leon'}`; // Obtiene la ciudad de la URL o muestra 'tu ciudad' si no está presente
+  const pageTitle = `Mantenimiento en ${router.query.city || 'Todo Nuevo Leon'}: Equipos y servicios para empresas.`; // Obtiene la ciudad de la URL o muestra 'tu ciudad' si no está presente
   return (
     <div className="bg-white text-black dark:bg-black dark:text-white">
       <script
@@ -42,10 +42,25 @@ const services = (
       ></script>
       <Layout
         title={pageTitle}
-        description={t('homepage:description')}
+        description="Mantenimiento regular de equipos y servicios para empresas en {estado}. Prevenimos fallas y costosas reparaciones, mejoramos la eficiencia y productividad de tu empresa"
         keywords="Services"
         baseUrl={"/sercices/maintenance"}
       >
+        <h1 className="font-bold text-5xl text-blue-700">{pageTitle}</h1>
+        <p className="text-lg py-2">Importancia del mantenimiento de equipos y servicios: "El mantenimiento regular de equipos y servicios es esencial para mantenerlos en buen estado y prolongar su vida útil. En nuestra empresa, sabemos lo importante que es para ti mantener tu negocio funcionando sin interrupciones y por eso ofrecemos servicios de mantenimiento de alta calidad para garantizar que tus equipos y servicios estén en óptimas condiciones".</p>
+        <p className="text-lg py-2">Importancia del mantenimiento de equipos y servicios: "El mantenimiento regular de equipos y servicios es esencial para mantenerlos en buen estado y prolongar su vida útil. En nuestra empresa, sabemos lo importante que es para ti mantener tu negocio funcionando sin interrupciones y por eso ofrecemos servicios de mantenimiento de alta calidad para garantizar que tus equipos y servicios estén en óptimas condiciones".</p>
+        <h2 className="font-bold text-3xl text-blue-700 py-2">Mantenimiento preventivo y correctivo de equipos de cómputo, redes y servidores.</h2>
+        <h2 className="font-bold text-3xl text-blue-700 py-2">Mantenimiento de aire acondicionado y sistemas eléctricos.</h2>
+        <div className="">
+        <h2 className="font-bold text-3xl text-blue-700 py-2">Ventajas</h2>
+        <ul>
+          <li className="list-disc text-lg">Precios competitivos.</li>
+          <li className="list-disc text-lg">Experiencia y profesionalismo en el área de mantenimiento.</li>
+          <li className="list-disc text-lg">Atención personalizada a las necesidades de cada cliente.</li>
+          <li className="list-disc text-lg">Servicio de calidad y garantía de satisfacción.</li>
+        </ul>
+        </div>
+
       </Layout>
     </div>
   );
